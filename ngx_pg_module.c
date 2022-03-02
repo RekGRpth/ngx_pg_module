ff3a8e5cf09abc4c7173137b3b90cfe383298099
RekGRpth <rekgrpth@gmail.com>
2022-03-02 11:37:15 +0500

    op

--- ngx_pg_module.c
+++ ngx_pg_module.c
@@ -225,7 +225,14 @@ static ngx_int_t ngx_pg_process_header(ngx_http_request_t *r) {
             b->pos += sizeof(uint32_t);
             switch (*b->pos++) {
                 case 'E': ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "TRANS_INERROR"); break;
-                case 'I': ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "TRANS_IDLE"); break;
+                case 'I': ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "TRANS_IDLE"); {
+                    ngx_pg_data_t *d = u->peer.data;
+                    u->request_bufs = d->request_bufs;
+                    d->request_bufs = NULL;
+                    ngx_int_t rc;
+                    if ((rc = ngx_output_chain(&u->output, u->request_bufs)) == NGX_ERROR) { ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "ngx_output_chain == NGX_ERROR"); return NGX_ERROR; }
+                    return NGX_AGAIN;
+                } break;
                 case 'T': ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "TRANS_INTRANS"); break;
                 default: ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "TRANS_UNKNOWN"); break;
             }
