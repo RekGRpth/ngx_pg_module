34aea1ce8a0e9f6579a5633a5e053e5322017077
RekGRpth <rekgrpth@gmail.com>
2022-03-21 16:44:16 +0500

    op

--- ngx_pg_module.c
+++ ngx_pg_module.c
@@ -709,7 +709,7 @@ inline static ngx_chain_t *ngx_pg_close(ngx_pool_t *p) {
     if (!(cl = close = ngx_pg_write_uint8(p, NULL, 'C'))) return NULL;
     if (!(cl = cl->next = cl_len = ngx_pg_alloc_len(p, &len))) return NULL;
     if (!(cl = cl->next = ngx_pg_write_uint8(p, &len, 'P'))) return NULL;
-    if (!(cl = cl->next = ngx_pg_write_uint8(p, &len, 0))) return NULL;
+    if (!(cl = cl->next = ngx_pg_write_str(p, &len, (ngx_str_t)ngx_string("")))) return NULL;
     cl_len->buf->last = pg_write_uint32(cl_len->buf->last, len);
     cl->next = NULL;
     return close;
