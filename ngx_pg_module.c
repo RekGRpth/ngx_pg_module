a713d11a906abae79acee0154790f2db98f67694
RekGRpth <rekgrpth@gmail.com>
2022-03-23 13:57:46 +0500

    op

--- ngx_pg_module.c
+++ ngx_pg_module.c
@@ -440,8 +440,7 @@ static ngx_chain_t *ngx_pg_bind(ngx_http_request_t *r) {
     if (!(cl = cl->next = cl_len = ngx_pg_alloc_len(p, &len))) return NULL;
     if (!(cl = cl->next = ngx_pg_write_str(p, &len, (ngx_str_t)ngx_string("")))) return NULL;
     if (!(cl = cl->next = ngx_pg_write_str(p, &len, (ngx_str_t)ngx_string("")))) return NULL;
-    if (!(cl = cl->next = ngx_pg_write_uint16(p, &len, plcf->arg->nelts))) return NULL;
-    for (ngx_uint_t i = 0; i < plcf->arg->nelts; i++) if (!(cl = cl->next = ngx_pg_write_uint16(p, &len, 0))) return NULL;
+    if (!(cl = cl->next = ngx_pg_write_uint16(p, &len, 0))) return NULL;
     if (!(cl = cl->next = ngx_pg_write_uint16(p, &len, plcf->arg->nelts))) return NULL;
     for (ngx_uint_t i = 0; i < plcf->arg->nelts; i++) {
         if (elts[i].complex.value.data) {
