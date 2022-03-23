6803d8f9c71171516f1599e80171daf6bdfafe22
RekGRpth <rekgrpth@gmail.com>
2022-03-23 08:36:43 +0500

    fix

--- ngx_pg_module.c
+++ ngx_pg_module.c
@@ -448,14 +448,14 @@ static ngx_int_t ngx_pg_peer_get(ngx_peer_connection_t *pc, void *data) {
     return NGX_DONE;
 }
 
-/*static void ngx_pg_read_handler(ngx_event_t *ev) {
+static void ngx_pg_read_handler(ngx_event_t *ev) {
     ngx_log_debug1(NGX_LOG_DEBUG_HTTP, ev->log, 0, "%s", __func__);
     ngx_connection_t *c = ev->data;
     ngx_pg_save_t *s = c->data;
 //    if (ngx_pg_parse(s) == NGX_OK) return;
     c->data = s->keep.data;
     s->keep.read_handler(ev);
-    c->data = s;
+    if (c->data == s->keep.data) c->data = s;
 }
 
 static void ngx_pg_write_handler(ngx_event_t *ev) {
@@ -464,8 +464,8 @@ static void ngx_pg_write_handler(ngx_event_t *ev) {
     ngx_pg_save_t *s = c->data;
     c->data = s->keep.data;
     s->keep.write_handler(ev);
-    c->data = s;
-}*/
+    if (c->data == s->keep.data) c->data = s;
+}
 
 static void ngx_pg_peer_free(ngx_peer_connection_t *pc, void *data, ngx_uint_t state) {
     ngx_log_debug1(NGX_LOG_DEBUG_HTTP, pc->log, 0, "state = %i", state);
@@ -482,12 +482,12 @@ static void ngx_pg_peer_free(ngx_peer_connection_t *pc, void *data, ngx_uint_t s
     ngx_log_debug1(NGX_LOG_DEBUG_HTTP, pc->log, 0, "s = %p", s);
     ngx_connection_t *c = s->connection;
     ngx_log_debug1(NGX_LOG_DEBUG_HTTP, pc->log, 0, "c = %p", c);
-//    s->keep.data = c->data;
-//    s->keep.read_handler = c->read->handler;
-//    s->keep.write_handler = c->write->handler;
-//    c->data = s;
-//    c->read->handler = ngx_pg_read_handler;
-//    c->write->handler = ngx_pg_write_handler;
+    s->keep.data = c->data;
+    s->keep.read_handler = c->read->handler;
+    s->keep.write_handler = c->write->handler;
+    c->data = s;
+    c->read->handler = ngx_pg_read_handler;
+    c->write->handler = ngx_pg_write_handler;
     s->pool->log = c->log;
     if (!pscf->log) return;
     c->log = pscf->log;
