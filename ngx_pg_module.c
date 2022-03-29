b0e5ceddef9f44930c2b5bda040244f0eb352e69
RekGRpth <rekgrpth@gmail.com>
2022-03-29 15:02:14 +0500

    op

--- ngx_pg_module.c
+++ ngx_pg_module.c
@@ -542,7 +542,7 @@ static ngx_chain_t *ngx_pg_bind(ngx_http_request_t *r) {
     ngx_pg_loc_conf_t *plcf = ngx_http_get_module_loc_conf(r, ngx_pg_module);
     ngx_pool_t *p = r->pool;
     uint32_t size = 0;
-    ngx_pg_arg_t *elts = plcf->cmd.arg->elts;
+    ngx_pg_arg_t *arg = plcf->cmd.arg->elts;
     if (!(cl = bind = ngx_pg_write_char(p, NULL, 'B'))) return NULL;
     if (!(cl = cl->next = cl_size = ngx_pg_alloc_size(p, &size))) return NULL;
     if (!(cl = cl->next = ngx_pg_write_str(p, &size, sizeof("") - 1, (u_char *)""))) return NULL;
@@ -550,9 +550,9 @@ static ngx_chain_t *ngx_pg_bind(ngx_http_request_t *r) {
     if (!(cl = cl->next = ngx_pg_write_int2(p, &size, 0))) return NULL;
     if (!(cl = cl->next = ngx_pg_write_int2(p, &size, plcf->cmd.arg->nelts))) return NULL;
     for (ngx_uint_t i = 0; i < plcf->cmd.arg->nelts; i++) {
-        if (elts[i].complex.value.data) {
+        if (arg[i].complex.value.data) {
             ngx_str_t value;
-            if (ngx_http_complex_value(r, &elts[i].complex, &value) != NGX_OK) { ngx_log_error(NGX_LOG_ERR, p->log, 0, "ngx_http_complex_value != NGX_OK"); return NULL; }
+            if (ngx_http_complex_value(r, &arg[i].complex, &value) != NGX_OK) { ngx_log_error(NGX_LOG_ERR, p->log, 0, "ngx_http_complex_value != NGX_OK"); return NULL; }
             if (!(cl = cl->next = ngx_pg_write_int4(p, &size, value.len))) return NULL;
             if (!(cl = cl->next = ngx_pg_write_byte(p, &size, value.len, value.data))) return NULL;
         } else {
