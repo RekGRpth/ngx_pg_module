6b4a6aec9b46c8f7beb9505205e47c83a1d5a8c2
RekGRpth <rekgrpth@gmail.com>
2022-03-30 12:54:47 +0500

    debug

--- ngx_pg_module.c
+++ ngx_pg_module.c
@@ -743,6 +743,7 @@ static ngx_int_t ngx_pg_peer_get(ngx_peer_connection_t *pc, void *data) {
         cl->buf = cmd->buf;
         ngx_buf_t *b = cl->buf;
         b->pos = b->start;
+        ngx_uint_t i = 0; for (u_char *p = cl->buf->pos; p < cl->buf->last; p++) ngx_log_debug3(NGX_LOG_DEBUG_HTTP, pc->log, 0, "%i:%i:%c", i++, *p, *p);
         if (cmd->next && !(cl = cl->next = ngx_alloc_chain_link(r->pool))) { ngx_log_error(NGX_LOG_ERR, pc->log, 0, "!ngx_alloc_chain_link"); return NGX_ERROR; }
     }
     cl->next = NULL;
