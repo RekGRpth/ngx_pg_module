34353a2da7732f875601653b65a316f8023168c9
RekGRpth <rekgrpth@gmail.com>
2022-04-15 11:07:05 +0500

    fix

--- ngx_pg_module.c
+++ ngx_pg_module.c
@@ -1230,7 +1230,7 @@ static ngx_int_t ngx_pg_input_filter(ngx_http_request_t *r, ssize_t bytes) {
     s->rc = NGX_OK;
     while (b->last < last && s->rc == NGX_OK) b->last += pg_fsm_execute(s->fsm, &ngx_pg_fsm_cb, s, b->last, last, b->end);
     ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "s->rc = %d", s->rc);
-    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "b->pos == b->last = %s", b->pos == b->last ? "true" : "false");
+    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "b->last == last = %s", b->last == last ? "true" : "false");
     if (!d->nqueries && s->state != pg_ready_for_query_state_unknown) u->length = 0;
     return s->rc;
 }
